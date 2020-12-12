e4c7d5633f998d8703ee2739063fe43d9c13152f
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2020-12-12 17:06:16 +0000

    theme toggle fixes

--- components/Header/index.js
+++ components/Header/index.js
@@ -26,7 +26,7 @@ const Header = () => {
             className='sm:px-8 transition duration-200 ease-in'
             onClick={() => setTheme(colorMode)}
           >
-            {colorMode === 'dark' ? (
+            {colorMode === 'light' ? (
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
